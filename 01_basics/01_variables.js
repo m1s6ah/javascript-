4a723cc58e43b7c6ada1b75bfe238b83caf8523b
m1s6ah <140872652+m1s6ah@users.noreply.github.com>
2026-06-10 02:20:40 +0000

    02_datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail ="misbah123@gmail.com"
+var accountPassword  = "12345"
+accountCity = "gujarat"
+
+//accountId = 2 not allowed
+
+// console.log(accountId);clea
+
+accountEmail = "hbcjn@g.com"
+accountPassword ="111111"
+accountCity = "bengluru"
+
+// console.log(accountId);
+
+//but if we want to print all the things together we will form a table
+
+console.table([accountEmail, accountId ,accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+// console.log(3+3); console.log("misbah");  dont do this because it is a bad codepratice instead
+ 
+console.log(3+3); 
+console.log("misbah"); 
+
+//datatypes
+
+let name = "hitesh" //string
+let age = 19 //num
+let isLoggedIm = false // bool
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof "hitesh");
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
