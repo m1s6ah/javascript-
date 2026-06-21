9d4a32c5c01ec6a246583f766e0e9958a1ae6bb0
m1s6ah <140872652+m1s6ah@users.noreply.github.com>
2026-06-21 03:17:40 +0000

    control flow topics

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,38 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "mar"
+// const month = 2
+
+switch (month){
+    case "mar":
+         console.log("jan");
+         
+         break;
+
+    case 2:
+         console.log("feb");
+         
+         break;
+
+    case 3:
+         console.log("mar");
+         
+        //  break;
+    
+    
+    case 4:
+         console.log("apr");
+         
+         break;
+    default:
+
+    console.log("default case match");
+    break;
+}
