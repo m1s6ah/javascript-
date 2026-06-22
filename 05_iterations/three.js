006baea1a6982f0625b26946747b7051c0d3dde5
m1s6ah <140872652+m1s6ah@users.noreply.github.com>
2026-06-22 15:29:30 +0000

    iterations

---
+++ 05_iterations/three.js
@@ -0,0 +1,33 @@
+// for of 
+//["","",""]
+// [{},{},{}]
+
+// const arr = [1 , 2 ,3 ,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greetings = "hello world!"
+// for (const greet of greetings){
+//     console.log(`each char is ${greet}`);
+    
+// }
+
+
+/////////////////// Maps
+
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('FR', "France")
+map.set('GR', "Germany")
+
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,':-',value);
+    
+}
