8b4ad5ef426afe878f00b55f2fbe9bbf65e3f6d2
m1s6ah <140872652+m1s6ah@users.noreply.github.com>
2026-06-23 12:26:56 +0000

    iterations

---
+++ 05_iterations/six.js
@@ -0,0 +1,36 @@
+// const coding =["js", "ruby","java","python","cpp"]
+
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+
+
+const myNums = [1,2,3,4,5,6,7,8]
+
+// const newNums = myNums.filter((num ) => num > 4)
+
+// now jab arrow function me curly brace ka istemal krenge toh return likhna laazmi hai kyuki woh scope bana leta hai toh scope k andar ki chiz without return likhe excess nhi hopaati
+
+// const newNums = myNums.filter((num ) => { return num > 4})
+
+
+
+/// using ForEach
+
+// const newNums = []
+
+// myNums.forEach( (num)=>
+//     {
+//         if (num > 4){
+//             newNums.push(num)
+//         }
+//     })
+
+// console.log(newNums);
+
+
+
